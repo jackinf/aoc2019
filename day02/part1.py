0fac6dd880569f378076c56316fc0f6c4423b6d3
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2019-12-02 09:48:01 +0100

    day02: part2

--- day02/part1.py
+++ day02/part1.py
@@ -1,16 +1,17 @@
 from typing import List
 
 
-def solve(inputs: List[int]) -> int:
+def solve_part1(inputs: List[int]) -> int:
     for i in range(0, len(inputs), 4):
         action = inputs[i]
+        if action == 99:
+            return inputs[0]
+
         first_pos = inputs[i+1]
         second_pos = inputs[i+2]
         output_pos = inputs[i+3]
 
-        if action == 99:
-            return inputs[0]
-        elif action == 1:
+        if action == 1:
             inputs[output_pos] = inputs[first_pos] + inputs[second_pos]
         elif action == 2:
             inputs[output_pos] = inputs[first_pos] * inputs[second_pos]
@@ -22,4 +23,4 @@ with open('input.txt', 'r') as f:
     file_inputs = [int(x) for x in f.readline().split(',')]
     file_inputs[1] = 12
     file_inputs[2] = 2
-    print(solve(file_inputs))
+    print(f'Solution to part 1: {solve_part1(file_inputs)}')
---
+++ day02/part2.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from day02.part1 import solve_part1
+
+
+def solve_part2(inputs: List[int]) -> int:
+    for i in range(0, 100):
+        for j in range(0, 100):
+            copied_inputs = inputs[:]
+            copied_inputs[1] = i
+            copied_inputs[2] = j
+            solution = solve_part1(copied_inputs)
+            if solution == 19690720:
+                return 100 * i + j
+    return -1
+
+
+with open('input.txt', 'r') as f:
+    file_inputs = [int(x) for x in f.readline().split(',')]
+    print(f'Solution to part 2: {solve_part2(file_inputs)}')
