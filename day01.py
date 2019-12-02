27709d776fcbb2bba408db63882bfd747cca541f
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2019-12-02 08:45:31 +0100

    day01

---
+++ day01.py
@@ -0,0 +1,17 @@
+def get_fuel_requirement(mass: int):
+    return mass // 3 - 2
+
+
+# Example:
+# print(get_fuel_requirement(73617))
+# print(get_fuel_requirement(14))
+# print(get_fuel_requirement(1969))
+# print(get_fuel_requirement(100756))
+
+
+file = open('day01-inputs.txt', 'r')
+with file as f:
+    answer = sum([get_fuel_requirement(int(x.strip())) for x in f.readlines()])
+print(answer)
+
+# Correct answer: 3271095
