e11b3ce1b26a5249d02f6990838f77724ea913d0
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2019-12-08 12:46:34 +0100

    day08 part2

--- day08/solution.py
+++ day08/solution.py
@@ -1,19 +1,34 @@
 import sys
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 
 def parse_inputs(test_case: str) -> List[int]:
     return [int(x) for x in list(test_case)]
 
 
+BLACK = 0
+WHITE = 1
+TR = 2  # transparent
+
 class Layer:
-    def __init__(self, index: int, grid: List[List[int]]):
+    def __init__(self, index: str, grid: List[List[int]]):
         self.index = index
         self.grid = grid
         self.flat = [x for sublist in grid for x in sublist]
 
     def get_number_of_elements(self, element: int):
         return self.flat.count(element)
+
+    def get_pixel(self, i: int, j: int):
+        return self.grid[j][i]
+
+    def decode_message(self) -> str:
+        res = ""
+        for cols in self.grid:
+            for item in cols:
+                res += str("     " if item == BLACK else "#####")
+            res += "\n"
+        return res
 
     def __str__(self):
         return f"\n<Layer {self.index} with {self.grid}>"
@@ -23,12 +38,17 @@ class Layer:
 
 
 class Solution:
-    def run(self, inputs: List[int], rows: int, cols: int) -> int:
+    def run(self, inputs: List[int], rows: int, cols: int) -> Tuple[int, str]:
         layers = self.build_layers(inputs, rows, cols)
-        print(f"Layers: {layers}")
+        # print(f"Layers: {layers}")
         layer = self.find_layer_with_fewest_zeros(layers)
-        print(f"Found layer: {layer}")
-        return self.get_product_of_ones_and_twos_in_layer(layer)
+        print(f"[PART 1] Found layer: {layer}")
+        merged_layer = self.merge_layers(layers, rows, cols)
+
+        print(f"[PART 2] Merged layer: {merged_layer}")
+        part1_result = self.get_product_of_ones_and_twos_in_layer(layer)
+        part2_result = merged_layer.decode_message()
+        return part1_result, part2_result
 
     def build_layers(self, inputs: List[int], rows: int, cols: int) -> List[Layer]:
         #   0     1      2     3    4     5
@@ -40,13 +60,10 @@ class Solution:
         layer_count = len(inputs) // layer_size
 
         for layer_i in range(layer_count):
-            arr = [[-1 for y in range(rows)] for x in range(cols)]
+            grid = self.build_empty_grid(rows, cols)
             for i in range(rows * cols):
-                row_i = i // rows
-                col_i = int(i % rows)
-                inputs_index = layer_i * layer_size + i
-                arr[row_i][col_i] = inputs[inputs_index]
-            layers.append(Layer(layer_i, arr))
+                grid[i // rows][int(i % rows)] = inputs[layer_i * layer_size + i]
+            layers.append(Layer(str(layer_i), grid))
 
         return layers
 
@@ -63,12 +80,33 @@ class Solution:
     def get_product_of_ones_and_twos_in_layer(self, layer: Layer) -> int:
         return layer.get_number_of_elements(1) * layer.get_number_of_elements(2)
 
+    def build_empty_grid(self, rows: int, cols: int):
+        return [[-1 for _ in range(rows)] for _ in range(cols)]
 
-test_case_1 = "123456789012"
+    def merge_layers(self, layers: List[Layer], rows: int, cols: int) -> Layer:
+        grid = self.build_empty_grid(rows, cols)
+        for i in range(rows):
+            for j in range(cols):
+                for layer in layers:
+                    pixel = layer.get_pixel(i, j)
+                    if pixel == WHITE or pixel == BLACK:
+                        grid[j][i] = pixel
+                        break
+                if grid[j][i] == -1:
+                    grid[j][i] = TR
+        return Layer("Merged", grid)
+
 
 sol = Solution()
-print(sol.run(parse_inputs(test_case_1), 3, 2))
+print("TEST CASE 1")
+print(sol.run(parse_inputs("123456789012"), 3, 2))
+print("\nTEST CASE 2")
+print(sol.run(parse_inputs("0222112222120000"), 2, 2))
 
 with open("input.txt", "r") as f:
     parsed_inputs = parse_inputs(f.readline())
-    print(sol.run(parsed_inputs, 25, 6))
+    print("\nSOLUTION")
+
+    part1, part2 = sol.run(parsed_inputs, 25, 6)
+    print(part1)
+    print(part2)
