8c7140a56c603578a6132efa958cb8922186c39f
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2019-12-09 15:23:29 +0100

    day07 part02 solved

--- day07/solution.py
+++ day07/solution.py
@@ -9,29 +9,54 @@ def parse_inputs(test_case: str) -> List[int]:
 
 
 class Solution:
-    def run(self, inputs: List[int]) -> int:
-        return self.run_sequence(inputs, [4, 3, 2, 1, 0])
-
-    def run_sequence(self, inputs: List[int], phases: List[int]) -> int:
-        sol5 = Intcode()
+    def run_part1(self, inputs: List[int]) -> int:
         max_output = 0
-        for permutation in permutations(phases):
+        for permutation in permutations([4, 3, 2, 1, 0]):
             last_output = 0
             for phase in permutation:
-                result1 = list(sol5.run(inputs[:], [phase, last_output]))
-                last_output = result1[-1]
+                result = list(Intcode(inputs[:], [phase, last_output]).run())
+                last_output = result[-1]
+            max_output = max(max_output, last_output)
+        return max_output
+
+    def run_part2(self, inputs: List[int]) -> int:
+        max_output = 0
+        for permutation in permutations([5, 6, 7, 8, 9]):
+            last_output = 0
+            intcodes = []
+            for phase in permutation:
+                intcodes.append(Intcode(inputs[:], [phase], break_on_output=True))
+
+            while any(intcode.done is False for intcode in intcodes):
+                for intcode in intcodes:
+                    intcode.input_values.append(last_output)
+                    result = list(intcode.run())
+                    if len(result) > 0:
+                        last_output = result[-1]
             max_output = max(max_output, last_output)
         return max_output
 
 
+sol = Solution()
+
+print("\nTEST CASES FOR PART 1")
 test_case_1 = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"  # 43210
 test_case_2 = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"  # 54321
 test_case_3 = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"  # 65210
 
-sol = Solution()
-print(sol.run(parse_inputs(test_case_1)))
-print(sol.run(parse_inputs(test_case_2)))
-print(sol.run(parse_inputs(test_case_3)))
+print(sol.run_part1(parse_inputs(test_case_1)))
+print(sol.run_part1(parse_inputs(test_case_2)))
+print(sol.run_part1(parse_inputs(test_case_3)))
+
+print("\nTEST CASES FOR PART 2")
+test_case_4 = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
+test_case_5 = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
+print(sol.run_part2(parse_inputs(test_case_4)))
+print(sol.run_part2(parse_inputs(test_case_5)))
 
 with open('input.txt', 'r') as f:
-    print(sol.run(parse_inputs(f.readline())))
+    parsed_inputs = parse_inputs(f.readline())
+    print("\nPART 1")
+    print(sol.run_part1(parsed_inputs))
+    print("\nPART 2")
+    print(sol.run_part2(parsed_inputs))
--- shared/Intcode.py
+++ shared/Intcode.py
@@ -1,59 +1,83 @@
 from typing import List, Generator
+from enum import IntEnum
+
+
+class OpCode(IntEnum):
+    ADD = 1
+    MULT = 2
+    INPUT = 3
+    OUTPUT = 4
+    JUMP_TRUE = 5
+    JUMP_FALSE = 6
+    LESSTHAN = 7
+    EQUAL = 8
+    HALT = 99
 
 
 class Intcode:
-    def run(self, inputs: List[int], input_values: List[int]) -> Generator[int, None, None]:
-        i = 0
-        input_value_index = 0
-        while inputs[i] != 99 and i <= len(inputs):
-            opcode = inputs[i]
+    def __init__(self, inputs: List[int], input_values: List[int], **kwargs):
+        self.inputs = inputs
+        self.input_values = input_values
+        self.break_on_output = kwargs['break_on_output'] if 'break_on_output' in kwargs else False
+        self.done = False
+        self.instruction_pointer = 0
+
+    def run(self) -> Generator[int, None, None]:
+        inputs = self.inputs
+        input_values = self.input_values
+
+        while self.instruction_pointer <= len(inputs):
+            opcode = inputs[self.instruction_pointer]
             first_pos_mode = 0
             second_pos_mode = 0
 
             if len(str(opcode)) > 1:
                 opcode, first_pos_mode, second_pos_mode = self.parse_opcode(opcode)
 
-            if opcode == 1:
-                first = self.get_position_or_immediate_value(inputs, i + 1, first_pos_mode)
-                second = self.get_position_or_immediate_value(inputs, i + 2, second_pos_mode)
-                inputs[inputs[i + 3]] = first + second
-                i += 4
-            elif opcode == 2:
-                first = self.get_position_or_immediate_value(inputs, i + 1, first_pos_mode)
-                second = self.get_position_or_immediate_value(inputs, i + 2, second_pos_mode)
-                inputs[inputs[i + 3]] = first * second
-                i += 4
-            elif opcode == 3:
-                inputs[inputs[i + 1]] = input_values[input_value_index]
-                if len(input_values) != input_value_index - 1:
-                    input_value_index += 1
-                i += 2
-            elif opcode == 4:
-                value = self.get_position_or_immediate_value(inputs, i + 1, first_pos_mode)
+            if opcode == OpCode.ADD:
+                first = self.get_position_or_immediate_value(inputs, self.instruction_pointer + 1, first_pos_mode)
+                second = self.get_position_or_immediate_value(inputs, self.instruction_pointer + 2, second_pos_mode)
+                inputs[inputs[self.instruction_pointer + 3]] = first + second
+                self.instruction_pointer += 4
+            elif opcode == OpCode.MULT:
+                first = self.get_position_or_immediate_value(inputs, self.instruction_pointer + 1, first_pos_mode)
+                second = self.get_position_or_immediate_value(inputs, self.instruction_pointer + 2, second_pos_mode)
+                inputs[inputs[self.instruction_pointer + 3]] = first * second
+                self.instruction_pointer += 4
+            elif opcode == OpCode.INPUT:
+                inputs[inputs[self.instruction_pointer + 1]] = input_values.pop(0)
+                self.instruction_pointer += 2
+            elif opcode == OpCode.OUTPUT:
+                value = self.get_position_or_immediate_value(inputs, self.instruction_pointer + 1, first_pos_mode)
                 yield value
-                i += 2
-            elif opcode == 5:
-                first = self.get_position_or_immediate_value(inputs, i + 1, first_pos_mode)
+                self.instruction_pointer += 2
+                if self.break_on_output:
+                    break
+            elif opcode == OpCode.JUMP_TRUE:
+                first = self.get_position_or_immediate_value(inputs, self.instruction_pointer + 1, first_pos_mode)
                 if first != 0:
-                    i = self.get_position_or_immediate_value(inputs, i + 2, second_pos_mode)
+                    self.instruction_pointer = self.get_position_or_immediate_value(inputs, self.instruction_pointer + 2, second_pos_mode)
                 else:
-                    i += 3
-            elif opcode == 6:
-                first = self.get_position_or_immediate_value(inputs, i + 1, first_pos_mode)
+                    self.instruction_pointer += 3
+            elif opcode == OpCode.JUMP_FALSE:
+                first = self.get_position_or_immediate_value(inputs, self.instruction_pointer + 1, first_pos_mode)
                 if first == 0:
-                    i = self.get_position_or_immediate_value(inputs, i + 2, second_pos_mode)
+                    self.instruction_pointer = self.get_position_or_immediate_value(inputs, self.instruction_pointer + 2, second_pos_mode)
                 else:
-                    i += 3
-            elif opcode == 7:
-                first = self.get_position_or_immediate_value(inputs, i + 1, first_pos_mode)
-                second = self.get_position_or_immediate_value(inputs, i + 2, second_pos_mode)
-                inputs[inputs[i + 3]] = 1 if first < second else 0
-                i += 4
-            elif opcode == 8:
-                first = self.get_position_or_immediate_value(inputs, i + 1, first_pos_mode)
-                second = self.get_position_or_immediate_value(inputs, i + 2, second_pos_mode)
-                inputs[inputs[i + 3]] = 1 if first == second else 0
-                i += 4
+                    self.instruction_pointer += 3
+            elif opcode == OpCode.LESSTHAN:
+                first = self.get_position_or_immediate_value(inputs, self.instruction_pointer + 1, first_pos_mode)
+                second = self.get_position_or_immediate_value(inputs, self.instruction_pointer + 2, second_pos_mode)
+                inputs[inputs[self.instruction_pointer + 3]] = 1 if first < second else 0
+                self.instruction_pointer += 4
+            elif opcode == OpCode.EQUAL:
+                first = self.get_position_or_immediate_value(inputs, self.instruction_pointer + 1, first_pos_mode)
+                second = self.get_position_or_immediate_value(inputs, self.instruction_pointer + 2, second_pos_mode)
+                inputs[inputs[self.instruction_pointer + 3]] = 1 if first == second else 0
+                self.instruction_pointer += 4
+            elif opcode == OpCode.HALT:
+                self.done = True
+                break
 
     def parse_opcode(self, opcode: int):
         param_mode = "{:05d}".format(opcode)
