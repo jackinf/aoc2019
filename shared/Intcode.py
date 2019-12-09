22c8dad960b64fa809eb0af399f9096ee911226b
jrumjantsev <jrumjantsev@travix.com>
2019-12-09 22:55:25 +0100

    intcode refactoring

--- shared/Intcode.py
+++ shared/Intcode.py
@@ -15,6 +15,12 @@ class OpCode(IntEnum):
     HALT = 99
 
 
+class PosMode(IntEnum):
+    POS = 0
+    IMMEDIATE = 1
+    RELATIVE = 2
+
+
 class Intcode:
     def __init__(self, registry: List[int], inputs: List[int], **kwargs):
         self.registry = registry + [0] * 1000
@@ -26,7 +32,6 @@ class Intcode:
 
     def run(self) -> Generator[int, None, None]:
         registry = self.registry
-        inputs = self.inputs
 
         while self.address <= len(registry):
             opcode = registry[self.address]
@@ -38,61 +43,70 @@ class Intcode:
                 opcode, first_pos_mode, second_pos_mode, third_pos_mode = self.parse_opcode(opcode)
 
             if opcode == OpCode.ADD:
-                addr0 = self.get_address_pointer(registry, self.address + 1, first_pos_mode)
-                addr1 = self.get_address_pointer(registry, self.address + 2, second_pos_mode)
-                addr2 = self.get_address_pointer(registry, self.address + 3, third_pos_mode)
+                addr0 = self.get_addr0(first_pos_mode)
+                addr1 = self.get_addr1(second_pos_mode)
+                addr2 = self.get_addr2(third_pos_mode)
                 registry[addr2] = registry[addr0] + registry[addr1]
                 self.address += 4
             elif opcode == OpCode.MULT:
-                addr0 = self.get_address_pointer(registry, self.address + 1, first_pos_mode)
-                addr1 = self.get_address_pointer(registry, self.address + 2, second_pos_mode)
-                addr2 = self.get_address_pointer(registry, self.address + 3, third_pos_mode)
+                addr0 = self.get_addr0(first_pos_mode)
+                addr1 = self.get_addr1(second_pos_mode)
+                addr2 = self.get_addr2(third_pos_mode)
                 registry[addr2] = registry[addr0] * registry[addr1]
                 self.address += 4
             elif opcode == OpCode.INPUT:
-                addr0 = self.get_address_pointer(registry, self.address + 1, first_pos_mode)
-                registry[addr0] = inputs.pop(0)
+                addr0 = self.get_addr0(first_pos_mode)
+                registry[addr0] = self.inputs.pop(0)
                 self.address += 2
             elif opcode == OpCode.OUTPUT:
-                addr0 = self.get_address_pointer(registry, self.address + 1, first_pos_mode)
+                addr0 = self.get_addr0(first_pos_mode)
                 yield self.registry[addr0]
                 self.address += 2
                 if self.break_on_output:
                     break
             elif opcode == OpCode.JUMP_TRUE:
-                addr0 = self.get_address_pointer(registry, self.address + 1, first_pos_mode)
+                addr0 = self.get_addr0(first_pos_mode)
+                addr1 = self.get_addr1(second_pos_mode)
                 if self.registry[addr0] != 0:
-                    addr1 = self.get_address_pointer(registry, self.address + 2, second_pos_mode)
                     self.address = self.registry[addr1]
                 else:
                     self.address += 3
             elif opcode == OpCode.JUMP_FALSE:
-                addr0 = self.get_address_pointer(registry, self.address + 1, first_pos_mode)
+                addr0 = self.get_addr0(first_pos_mode)
+                addr1 = self.get_addr1(second_pos_mode)
                 if self.registry[addr0] == 0:
-                    addr1 = self.get_address_pointer(registry, self.address + 2, second_pos_mode)
                     self.address = self.registry[addr1]
                 else:
                     self.address += 3
             elif opcode == OpCode.LESSTHAN:
-                addr0 = self.get_address_pointer(registry, self.address + 1, first_pos_mode)
-                addr1 = self.get_address_pointer(registry, self.address + 2, second_pos_mode)
-                addr2 = self.get_address_pointer(registry, self.address + 3, third_pos_mode)
+                addr0 = self.get_addr0(first_pos_mode)
+                addr1 = self.get_addr1(second_pos_mode)
+                addr2 = self.get_addr2(third_pos_mode)
                 registry[addr2] = 1 if registry[addr0] < registry[addr1] else 0
                 self.address += 4
             elif opcode == OpCode.EQUAL:
-                addr0 = self.get_address_pointer(registry, self.address + 1, first_pos_mode)
-                addr1 = self.get_address_pointer(registry, self.address + 2, second_pos_mode)
-                addr2 = self.get_address_pointer(registry, self.address + 3, third_pos_mode)
+                addr0 = self.get_addr0(first_pos_mode)
+                addr1 = self.get_addr1(second_pos_mode)
+                addr2 = self.get_addr2(third_pos_mode)
                 registry[addr2] = 1 if registry[addr0] == registry[addr1] else 0
                 self.address += 4
             elif opcode == OpCode.BASE:
-                addr0 = self.get_address_pointer(registry, self.address + 1, first_pos_mode)
+                addr0 = self.get_addr0(first_pos_mode)
                 self.base += registry[addr0]
                 self.address += 2
                 pass
             elif opcode == OpCode.HALT:
                 self.done = True
                 break
+
+    def get_addr0(self, pos_mode: int):
+        return self.get_address_pointer(self.address + 1, pos_mode)
+
+    def get_addr1(self, pos_mode: int):
+        return self.get_address_pointer(self.address + 2, pos_mode)
+
+    def get_addr2(self, pos_mode: int):
+        return self.get_address_pointer(self.address + 3, pos_mode)
 
     def parse_opcode(self, opcode: int):
         param_mode = "{:05d}".format(opcode)
@@ -102,10 +116,10 @@ class Intcode:
         third_pos_mode = int(param_mode[0])
         return p_opcode, first_pos_mode, second_pos_mode, third_pos_mode
 
-    def get_address_pointer(self, arr: List[int], index: int, pos_mode: int) -> int:
-        if pos_mode == 0:
-            return arr[index]
-        if pos_mode == 1:
+    def get_address_pointer(self, index: int, pos_mode: int) -> int:
+        if pos_mode == PosMode.POS:
+            return self.registry[index]
+        if pos_mode == PosMode.IMMEDIATE:
             return index
-        if pos_mode == 2:
-            return arr[index] + self.base
+        if pos_mode == PosMode.RELATIVE:
+            return self.registry[index] + self.base
