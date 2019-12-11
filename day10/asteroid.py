fbb70f15822b8c1bd85f17ee1b33b45578f024df
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2019-12-11 13:06:02 +0100

    day10 part01 - finally solved this

--- day10/asteroid.py
+++ day10/asteroid.py
@@ -1,13 +1,18 @@
+import math
+from typing import Tuple, List
+
+
 class Asteroid:
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
         self.seen_asteroids = []
         self.blocked_asteroids = []
+        self.other_asteroids: List[Tuple[Asteroid, str, float]] = []
 
     def has_seen(self, x: int, y: int) -> bool:
         for seen_asteroid in self.seen_asteroids:
-            if seen_asteroid[0] == x and seen_asteroid[1] == y:
+            if seen_asteroid.x == x and seen_asteroid.y == y:
                 return True
         return False
 
@@ -17,11 +22,38 @@ class Asteroid:
                 return True
         return False
 
+    def add_other_asteroid(self, other):
+        angle = f'{round(math.atan2(other.x - self.x, other.y - self.y), 4)}'
+        dist = round(math.sqrt((other.x - self.x)**2 + (other.y - self.y)**2), 4)
+        # for _, other_angle, other_distance in self.other_asteroids:
+        #     if other_angle == angle and other_distance < dist:
+        #         return  # found an asteroid with smaller distance
+        if dist == 0:
+            return
+        self.other_asteroids.append((other, angle, dist))
+
+    def clean_up_blocked(self):
+        temp = {}
+        for asteroid, angle, dist in self.other_asteroids:
+            if angle in temp:
+                if math.fabs(temp[angle][0]) > math.fabs(dist):
+                    temp[angle] = (dist, asteroid)
+            else:
+                temp[angle] = (dist, asteroid)
+
+        self.other_asteroids.clear()
+        for angle, dist_asteroid in temp.items():
+            dist, asteroid = dist_asteroid
+            self.other_asteroids.append((asteroid, angle, dist))
+
     def get_coord(self):
         return self.x, self.y
 
     def get_how_much_asteroids_i_see(self):
         return f'({self.x}, {self.y}): I see {len(self.seen_asteroids)} asteroids.'
+
+    def get_how_much_other_asteroids_i_see(self):
+        return f'({self.x}, {self.y}): I see {len(self.other_asteroids)} asteroids.'
 
     def is_same(self, asteroid) -> bool:
         return self.x == asteroid.x and self.y == asteroid.y
--- day10/solution_attempt2.py
+++ day10/solution_attempt2.py
@@ -31,135 +31,29 @@ class AsteroidManager:
             self.find_asteroids_for_current(grid, asteroid)
 
     def find_asteroids_for_current(self, grid: Grid, current: Asteroid):
-        rows_count, cols_count = grid.get_rows_cols_count()
-        current.seen_asteroids.clear()
-        current.blocked_asteroids.clear()
-
-        left_vertical = [(current.x-1, i) for i in range(rows_count)]
-        right_vertical = [(current.x+1, i) for i in range(rows_count)]
-        upper_horizontal = [(i, current.y-1) for i in range(cols_count)]
-        lower_horizontal = [(i, current.y+1) for i in range(cols_count)]
-
-        combined = left_vertical + right_vertical + upper_horizontal + lower_horizontal
-
-        # find all blocked asteroids
-        for start_x, start_y in combined:
-            delta_x, delta_y = start_x - current.x, start_y - current.y
-            x, y = current.x + delta_x, current.y + delta_y
-            found = False
-            while 0 <= x < cols_count and 0 <= y < rows_count:
-                if grid.has_asteroid_at(x, y) and not current.is_blocked(x, y):
-                    if found:
-                        current.blocked_asteroids.append(grid.get(x, y))
-                    else:
-                        found = True
-                x, y = x + delta_x, y + delta_y
-
-        # find all rest asteroids which are not blocked
         for asteroid in grid.get_all_asteroids():
-            if not current.is_blocked(asteroid.x, asteroid.y) and \
-                    not current.has_seen(asteroid.x, asteroid.y) and \
-                    not current.is_same(asteroid):
-                current.seen_asteroids.append([asteroid.x, asteroid.y])
+            current.add_other_asteroid(asteroid)
+        current.clean_up_blocked()
 
     def ask_all_asteroids_how_much_they_see(self, grid: Grid) -> Tuple[Union[None, Asteroid], int]:
         max_asteroids_seen = 0
         max_asteroid: Union[None, Asteroid] = None
-        for line in grid.cols:
-            for item in line:
-                if item is not None:
-                    print(item.get_how_much_asteroids_i_see())
-                    if max_asteroids_seen < len(item.seen_asteroids):
-                        max_asteroids_seen = len(item.seen_asteroids)
-                        max_asteroid = item
+        for asteroid in grid.get_all_asteroids():
+            # print(asteroid.get_how_much_other_asteroids_i_see())
+            if max_asteroids_seen < len(asteroid.other_asteroids):
+                max_asteroids_seen = len(asteroid.other_asteroids)
+                max_asteroid = asteroid
         return max_asteroid, max_asteroids_seen
 
 
-case_1 = """
-.#.
-...
-###"""
-
-# (3, 4) has 8 asteroids
-case_2 = """
-.#..#
-.....
-#####
-....#
-...##"""
-
-# Best is 5,8 with 33 other asteroids detected:
-case_3 = """
-......#.#.
-#..#.#....
-..#######.
-.#.#.###..
-.#..#.....
-..#....#.#
-#..#....#.
-.##.#..###
-##...#..#.
-.#....####"""
-
-case_4 = """
-#.#...#.#.
-.###....#.
-.#....#...
-##.#.#.#.#
-....#.#.#.
-.##..###.#
-..#...##..
-..##....##
-......#...
-.####.###."""
-
-case_5 = """
-.#..#..###
-####.###.#
-....###.#.
-..###.##.#
-##.##.#.#.
-....###..#
-..#.#..#.#
-#..#.#.###
-.##...##.#
-.....#.#.."""
-
-case_6 = """
-.#..##.###...#######
-##.############..##.
-.#.######.########.#
-.###.#######.####.#.
-#####.##.#.##.###.##
-..#####..#.#########
-####################
-#.####....###.#.#.##
-##.#################
-#####.##.###..####..
-..######..##.#######
-####.##.####...##..#
-.#####..#.######.###
-##...#.##########...
-#.##########.#######
-.####.#.###.###.#.##
-....##.##.###..#####
-.#.#.###########.###
-#.#.#.#####.####.###
-###.##.####.##.#..##"""
-
 am = AsteroidManager()
-matrix1 = am.build_matrix(case_3)
-# print(matrix1)
-grid1 = am.build_grid(matrix1)
-grid1.print_grid()
 
-asteroid12 = grid1.get(0, 8)
-print(asteroid12)
-am.find_asteroids_for_current(grid1, asteroid12)
-print(asteroid12.blocked_asteroids)
-print(asteroid12.seen_asteroids)
+with open('input.txt', 'r') as f:
+    matrix1 = am.build_matrix('\n' + ''.join(f.readlines()))
+    grid1 = am.build_grid(matrix1)
+    # grid1.print_grid()
 
-am.find_asteroids_for_all(grid1)
-print(am.ask_all_asteroids_how_much_they_see(grid1))
-
+    print("PART 1")
+    am.find_asteroids_for_all(grid1)
+    print(am.ask_all_asteroids_how_much_they_see(grid1))
 
