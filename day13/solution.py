b6df39cc71fda38d648197d128586f37e07e9129
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2019-12-14 17:32:21 +0100

    day13 part02 - wip, brainstorming

--- day13/solution.py
+++ day13/solution.py
@@ -27,10 +27,13 @@ class Cell:
 
 class Arcade:
     def __init__(self, registry: List[int]):
-        self.intcode = Intcode(registry, [])
+        registry[0] = 2
+        self.intcode = Intcode(registry, [0])
         self.cells: List[Cell] = []
         self.cells1: Dict[Tuple[int, int], Tile] = {}
         self.next_action = CollectCellAction.GetX
+        self.last_ball_pos = None
+        self.paddle_x = None
 
     def collect_cells(self):
         x = None
@@ -45,8 +48,23 @@ class Arcade:
                 y = output
             elif self.next_action == CollectCellAction.GetTile:
                 self.next_action = CollectCellAction.GetX
-                self.cells.append(Cell(x, y, output))
-                self.cells1[(x, y)] = output
+                if output == Tile.Block:
+                    self.cells.append(Cell(x, y, output))
+                elif output == Tile.HorizontalPaddle:
+                    print(f'Horizontal paddle: ({x}, {y})')
+                    self.paddle_x = x
+
+                if x == -1 and y == 0:
+                    print(output)
+
+                if output == Tile.Ball:
+                    print(f"Ball: ({x}, {y})")
+                    diff = 0 if self.paddle_x is None else x - self.paddle_x
+                    print(f'Diff: {diff}')
+                    self.intcode.append_input(diff)
+                    self.paddle_x = x
+                else:
+                    self.intcode.append_input(0)
 
     def count_block_tiles(self):
         return len([cell for cell in self.cells if cell.tile == Tile.Block])
@@ -56,4 +74,5 @@ with open('input.txt', 'r') as f:
     registry = [int(x) for x in f.readline().split(',')]
     arcade = Arcade(registry)
     arcade.collect_cells()
+
     print(arcade.count_block_tiles())
