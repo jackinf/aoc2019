9b5eee3529456943b36491848d4e16d4187bf6c2
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2019-12-15 13:41:13 +0100

    day14 part01 - wip

--- day14/solution.py
+++ day14/solution.py
@@ -1,4 +1,6 @@
 from typing import List
+from collections import defaultdict
+import math
 
 
 class Chemical:
@@ -36,32 +38,46 @@ class Lab:
     def __init__(self, target_chemicals: List[TargetChemical]):
         self.target_chemicals = target_chemicals
 
-    def part1_calculate_ore(self, fuel: TargetChemical):
-        simple_chemicals = self.get_simple_chemicals(fuel)
+    def part1_calculate_ore(self):
+        fuel = next(x for x in target_chemicals if x.name == "FUEL")
+        simple_chemicals = self.get_simple_chemicals(fuel, 1)
         print(simple_chemicals)
 
-    def get_simple_chemicals(self, target_chemical: TargetChemical) -> List[TargetChemical]:
+        grouped = defaultdict(int)
+        for item in simple_chemicals:
+            grouped[item.name] += item.count
+        print(grouped)
+
+        ore_count = 0
+        for _, (checmical_name, chemical_count) in enumerate(grouped.items()):
+            target_chemical = self.find_target_chemical_by_name(checmical_name)
+            if not (len(target_chemical.source_chemicals) == 1 and target_chemical.source_chemicals[0].name == "ORE"):
+                raise Exception("I messed up! This is not a chemical which contains only ORE")
+            target_chemical_ore_count = target_chemical.source_chemicals[0].count
+            ore_count += math.ceil(chemical_count / target_chemical.count) * target_chemical_ore_count
+
+        print(f'Ore count needed: {ore_count}')
+
+    def get_simple_chemicals(self, target_chemical: TargetChemical, count: int) -> List[Chemical]:
         source_chemicals = target_chemical.source_chemicals
         if len(source_chemicals) == 0:
             raise Exception("Each chemical requires at least 1 source chemical")
 
         # basic case: "simple" chemicals can be created from ORE
         if len(source_chemicals) == 1 and source_chemicals[0].name == "ORE":
-            return [target_chemical]
+            return [Chemical(target_chemical.name, count)]
 
         simple_chemicals = []
-        for current_source_chemical in source_chemicals:
-            current_target_chemical = self.find_target_chemical_by_name(current_source_chemical)
-            found_simple_chemicals = self.get_simple_chemicals(current_target_chemical)
-            simple_chemicals.append(*found_simple_chemicals)
-
+        for source_chemical in source_chemicals:
+            next_target = self.find_target_chemical_by_name(source_chemical.name)
+            simple_chemicals += self.get_simple_chemicals(next_target, source_chemical.count * count)
         return simple_chemicals
 
-    def find_target_chemical_by_name(self, chemical: Chemical):
-        return next(x for x in self.target_chemicals if x.name == chemical.name)
+    def find_target_chemical_by_name(self, chemical_name: str) -> TargetChemical:
+        return next(x for x in self.target_chemicals if x.name == chemical_name)
 
 
-with open('test-input-1.txt', 'r') as f:
+with open('test-input-2.txt', 'r') as f:
     target_chemicals: List[TargetChemical] = []
     for line in f.readlines():
         line_split = line.split('=>')
@@ -78,5 +94,6 @@ with open('test-input-1.txt', 'r') as f:
         target_chemicals.append(target_chemical)
 
     print(target_chemicals)
-    fuel = next(x for x in target_chemicals if x.name == "FUEL")
-    print(fuel)
+
+    lab = Lab(target_chemicals)
+    lab.part1_calculate_ore()
