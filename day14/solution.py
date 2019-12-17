88c1826e9d7da389143046cde54b1a349c103a57
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2019-12-17 14:54:52 +0100

    day14 part01 - solution

--- day14/solution.py
+++ day14/solution.py
@@ -1,6 +1,6 @@
 import copy
 import math
-from typing import List
+from typing import List, Set
 
 
 class ReqElement:
@@ -23,7 +23,7 @@ class TargetElem:
 
 
 def collect_values():
-    with open('test-input-2.txt', 'r') as f:
+    with open('input.txt', 'r') as f:
         target_elements: List[TargetElem] = []
         for line in f.readlines():
             left, right = line.split(' => ')
@@ -39,14 +39,36 @@ def collect_values():
         return target_elements
 
 
+def calculate_all_requirements(target_elements: List[TargetElem], requirements: List[ReqElement]) -> Set[str]:
+    if len(requirements) == 0:
+        return set()
+
+    acc: Set[str] = set()
+    for requirement in requirements:
+        next_requirements = next((x.requirements for x in target_elements if x.name == requirement.name and x.name != "ORE"), None)
+        if next_requirements is None:
+            continue
+        acc.update([x.name for x in next_requirements if x.name != "ORE"])
+        acc.update(calculate_all_requirements(target_elements, next_requirements))
+    return acc
+
+
 def calculate_ore(target_elements: List[TargetElem]):
     fuel_element: TargetElem = next(x for x in target_elements if x.name == "FUEL")
+
+    # this will be handled like a queue: first elements will be popped, and new ones appended to the end
     requirements = copy.deepcopy(fuel_element.requirements)
 
     while [x.name for x in requirements] != ["ORE"]:
         # pop the current requirement
         current_requirement = [x for x in requirements if x.name != "ORE"].pop(0)
         requirements = [x for x in requirements if x.name != current_requirement.name]
+
+        # check if this element is still required for some different chemical reaction
+        all_dependencies = list(calculate_all_requirements(target_elements, requirements))
+        if current_requirement.name in all_dependencies:
+            requirements.append(current_requirement)  # add to the end
+            continue
 
         target_element = next(x for x in target_elements if x.name == current_requirement.name)
         for target_requirement in target_element.requirements:
