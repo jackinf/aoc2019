06c180ee746785885b8535bf890ddfc14894f94e
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2019-12-29 11:10:44 +0100

    day14 part01 - ability to take the key

--- day18/attempt1.py
+++ day18/attempt1.py
@@ -1,9 +1,11 @@
 import copy
-import sys
 from typing import List, Union, Tuple
 import numpy as np
 
 Grid = List[List[str]]
+PLAYER_CHAR = "@"
+FLOOR_CHAR = "."
+WALL_CHAR = "#"
 
 
 def print_grid(grid: Grid):
@@ -18,12 +20,35 @@ def collect_input(file_name) -> Grid:
         return [[x for x in line if x != "\n"] for line in f.readlines()]
 
 
-def get_letter_locations(grid: Grid):
+def get_all_letter_with_coordinates(grid: Grid) -> List[Tuple[int, int, str]]:
     arr = np.array(grid)
-    letters = [x for x in arr.flatten() if x not in [".", "#"]]
-    upper_letters = [x for x in letters if x.isupper()]
-    lower_letters = [x for x in letters if not x.isupper()]
-    return upper_letters, lower_letters
+
+    # mark all excluded values with x and filter x out. TODO: find a better way
+    arr = np.where(arr == ".", "x", arr)
+    arr = np.where(arr == "#", "x", arr)
+    arr = np.where(arr == "@", "x", arr)
+    res = np.where(arr != "x")
+
+    coordiates = list(zip(res[0], res[1]))
+    return [(x, y, grid[x][y]) for x, y in coordiates]
+
+
+def get_keys(coordinates: List[Tuple[int, int, str]]) -> List[Tuple[int, int, str]]:
+    return [coord for coord in coordinates if coord[2].islower()]
+
+
+def get_doors(coordinates: List[Tuple[int, int, str]]) -> List[Tuple[int, int, str]]:
+    return [coord for coord in coordinates if coord[2].isupper()]
+
+
+def unlock_door(grid: Grid, letter: str):
+    coordinates = get_all_letter_with_coordinates(grid)
+    the_key = next((key for key in get_keys(coordinates) if key[2] == letter))
+    the_door = next(door for door in get_doors(coordinates) if door[2] == letter.upper())
+    grid[the_key[0]][the_key[1]] = PLAYER_CHAR
+    grid[the_door[0]][the_door[1]] = FLOOR_CHAR
+    p_x, p_y = find_player_pos(grid)
+    grid[p_x][p_y] = "."
 
 
 def simplity_grid(grid: Grid) -> Grid:
@@ -79,10 +104,11 @@ def get_distance_to_letter(grid: Grid, x: int, y: int, letter: str, distance: in
 
 if __name__ == "__main__":
     grid = collect_input("test-case-1.txt")
-    print_grid(grid)
-    print(get_letter_locations(grid))
-    print_grid(grid)
 
+    print_grid(grid)
     player_x, player_y = find_player_pos(grid)
-    b_dist = get_distance_to_letter(copy.deepcopy(grid), player_x, player_y, 'b', 0, [])
+    b_dist = get_distance_to_letter(copy.deepcopy(grid), player_x, player_y, 'a', 0, [])
+    unlock_door(grid, 'a')
+
+    print_grid(grid)
     print(b_dist)
